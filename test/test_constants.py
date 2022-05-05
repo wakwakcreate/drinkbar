216b77cb47e7206bff4ae71f80f6d0fcf2acf97a
monman53 <monman.cs@gmail.com>
2022-05-05 23:50:46 +0900

    Add dummy user id test

--- test/test_constants.py
+++ test/test_constants.py
@@ -5,3 +5,7 @@ def test_drinks():
     assert(DRINK_ORANGE == 1)
     assert(DRINK_OOLONG == 2)
     assert(DRINK_JASMINE == 3)
+
+def test_dummy_user_id():
+    assert(DUMMY_USER_ID0 == 'Uxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    assert(DUMMY_USER_ID1 == 'Uyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')
