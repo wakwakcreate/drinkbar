141fea6e12745b6d63e9a8ad858b4cece13c7885
monman53 <monman.cs@gmail.com>
2022-05-05 23:50:46 +0900

    Implement answer selection

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+# Constants
 STATE_USER_SELECT = 10
 STATE_USER0_JOIN = 11
 STATE_USER1_JOIN = 12
@@ -38,25 +39,75 @@ STATE_USER2_JOIN = 13
 GAME_EASY = 0
 GAME_HARD = 1
 
-# Constants
-c = {
-    'scripts': {},
-}
+DRINK_MELON = 0
+DRINK_ORANGE = 1
+DRINK_OOLONG = 2
+DRINK_JASMINE = 3
+
+DUMMY_USER_ID0 = 'Uxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+DUMMY_USER_ID1 = 'Uyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+
+def get_drink_name(drink_id):
+    if drink_id == DRINK_MELON:
+        return "メロンソーダ"
+    elif drink_id == DRINK_ORANGE:
+        return "オレンジソーダ"
+    elif drink_id == DRINK_OOLONG:
+        return "ウーロン茶"
+    elif drink_id == DRINK_JASMINE:
+        return "ジャスミン茶"
+
+def get_user_name(group_id, user_id):
+    # Dummy names
+    if user_id == DUMMY_USER_ID0:
+        return "Aマン"
+    if user_id == DUMMY_USER_ID1:
+        return "Bマン"
+    
+    # Get user name via LINE API
+    profile = line_bot_api.get_group_member_profile(group_id, user_id)
+    return profile.display_name
+
+scripts = {}
 
 def load_scripts():
     # お題の選択肢
     url = "https://raw.githubusercontent.com/wakwakcreate/drink_scripts/main/scenario.csv"
-    c.scripts['scenario'] = pd.read_csv(url)
+    scenarios_csv = pd.read_csv(url)
+    scenarios = []
+    for _, row in scenarios_csv.iterrows():
+        answers = [row['ans0'], row['ans1'], row['ans2']]
+        scenario = {
+            'question': row['question'],
+            'answers': answers,
+        }
+        scenarios.append(scenario)
+    scripts['scenarios'] = scenarios
 
     # 個別に送るメッセージ
     url = "https://raw.githubusercontent.com/wakwakcreate/drink_scripts/main/mission.csv"
-    c.scripts['mission'] = pd.read_csv(url)
+    missions_csv = pd.read_csv(url)
+    missions = []
+    for _, row in missions_csv.iterrows():
+        missions.append(row['mission'])
+    scripts['missions'] = missions
 
     # ヘンテコミッション（easy, hard）
     url = "https://raw.githubusercontent.com/wakwakcreate/drink_scripts/main/easymission.csv"
-    c.cscripts['easymission'] = pd.read_csv(url)
+    easy_missions_csv = pd.read_csv(url)
+    easy_missions = []
+    for _, row in easy_missions_csv.iterrows():
+        easy_missions.append(row['mission'])
+    scripts['easy_missions'] = easy_missions
+
     url = "https://raw.githubusercontent.com/wakwakcreate/drink_scripts/main/hardmission.csv"
-    c.scripts['hardmission'] = pd.read_csv(url)
+    hard_missions_csv = pd.read_csv(url)
+    hard_missions = []
+    for _, row in hard_missions_csv.iterrows():
+        hard_missions.append(row['mission'])
+    scripts['hard_missions'] = hard_missions
+
+    print("Scripts loaded.")
 
 
 # Main callback
@@ -80,10 +131,11 @@ def callback():
     return 'OK'
 
 def reply_init_message(token, group_id):
+    load_scripts()
+
     game = {}
     game['state'] = STATE_USER_SELECT
-    game['group_id'] = group_id
-    game['debug_mode'] = False
+    game['users'] = []
     game_str = json.dumps(game)
 
     text = "一人目の参加者はボタンを押してね。"
@@ -94,11 +146,14 @@ def reply_init_message(token, group_id):
     line_bot_api.reply_message(token, selection_message)
 
 def start_debug_mode(token, group_id):
+    load_scripts()
+
     game = {}
     game['state'] = STATE_USER1_JOIN
-    game['group_id'] = group_id
-    game['user_ids'] = ["dummy_user_id_0", "dummy_user_id_1"]
-    game['debug_mode'] = True
+    game['users'] = [
+        {'id': DUMMY_USER_ID0}, 
+        {'id': DUMMY_USER_ID1},
+    ]
     game_str = json.dumps(game)
 
     text = "三人目の参加者はボタンを押してね。"
@@ -120,11 +175,11 @@ def handle_message(event):
     group_id = source.group_id
 
     # デバッグコマンドを処理
-    if event.message.text == "init":
+    if event.message.text == "i":
         reply_init_message(event.reply_token, group_id)
         return
     
-    if event.message.text == "debug":
+    if event.message.text == "d":
         start_debug_mode(event.reply_token, group_id)
         return
 
@@ -132,14 +187,14 @@ def handle_message(event):
 @ handler.add(PostbackEvent)
 def handle_postback(event):
     source = event.source
-    # group_id = source.group_id
+    group_id = source.group_id
     user_id = source.user_id
 
     game_str = event.postback.data
     game = json.loads(game_str)
 
     if game['state'] == STATE_USER_SELECT:
-        game['user_ids'] = [user_id]
+        game['users'].append({'id': user_id})
         game['state'] = STATE_USER0_JOIN
         game_str = json.dumps(game)
 
@@ -152,10 +207,11 @@ def handle_postback(event):
 
     elif game['state'] == STATE_USER0_JOIN:
         # Ignore duplicate join button click
-        if (user_id in game['user_ids']):
-            return
+        for user in game['users']:
+            if user['id'] == user_id:
+                return
         
-        game['user_ids'].append(user_id)
+        game['users'].append({'id': user_id})
         game['state'] = STATE_USER1_JOIN
         game_str = json.dumps(game)
 
@@ -168,10 +224,11 @@ def handle_postback(event):
 
     elif game['state'] == STATE_USER1_JOIN:
         # Ignore duplicate join button click
-        if (user_id in game['user_ids']):
-            return
+        for user in game['users']:
+            if user['id'] == user_id:
+                return
         
-        game['user_ids'].append(user_id)
+        game['users'].append({'id': user_id})
         game['state'] = STATE_USER2_JOIN
 
         game_easy = copy.deepcopy(game)
@@ -192,6 +249,77 @@ def handle_postback(event):
 
         line_bot_api.reply_message(event.reply_token, selection_message)
 
+    elif game['state'] == STATE_USER2_JOIN:
+        # ドリンクの配役をランダムに決定
+        drinks = [DRINK_MELON, DRINK_ORANGE]
+        drinks.append(random.choice([DRINK_OOLONG, DRINK_JASMINE]))
+        random.shuffle(drinks)
+        for idx, user in enumerate(game['users']):
+            user['drink'] = drinks[idx]
+
+        # お題をランダムに決定
+        scenarios = scripts['scenarios']
+        scenario_id = random.randint(0, len(scenarios) - 1)
+        game['scenario_id'] = scenario_id
+
+        # チャポンの選択肢をランダムに決定
+        game['chapon_id'] = random.randint(0, 2)
+
+        # へんてこミッションをランダムに決定
+        hentekos = scripts['easy_missions'] if game['difficulty'] == GAME_EASY else scripts['hard_missions']
+        henteko_id = random.randint(0, len(hentekos) - 1)
+        game['henteko_id'] = henteko_id
+
+        # 個別メッセージを送信
+        for user in game['users']:
+            user_id = user['id']
+            user_name = get_user_name(group_id, user_id)
+            drink_id = user['drink']
+            drink_name = get_drink_name(user['drink'])
+            mission = scripts['missions'][drink_id]
+
+            message = f"あなた（{user_name}）に{drink_name}が乗り移ったぞ。\n"
+            message += f"\n{drink_name}のあなたは、{mission}\n"
+            message += f"これが勝利条件だ！"
+            if drink_id == DRINK_JASMINE:
+                message += f"\nへんてこミッション：『{hentekos[henteko_id]}』\n"
+            if drink_id == DRINK_MELON:
+                answers = scenarios[scenario_id]['answers']
+                chapon_ans = answers[game['chapon_id']]
+                message += f"\nチャポンの選択肢は「{chapon_ans}」だ。チャポンの選択肢が何かはあなたしか知らないぞ。\n"
+            else:
+                message += f"\nチャポンの選択肢はメロンソーダの人しか知らない。メロンソーダに騙されるな。\n"
+
+            # Do not send message to dummy users
+            if user_id == DUMMY_USER_ID0 or user_id == DUMMY_USER_ID1:
+                continue
+
+            # NOTICE: This consumes API call count
+            line_bot_api.push_message(
+                user_id, TextSendMessage(text=message))
+        
+        # お題選択肢ボタンメッセージ
+        scenario = scenarios[scenario_id]
+        question = scenario['question']
+        actions = []
+        for i in range(3):
+            answer = scenario['answers'][i]
+            next_game = copy.deepcopy(game)
+            next_game['selected_scenario_id'] = i
+            next_game_str = json.dumps(next_game)
+            actions.append(PostbackAction(answer, next_game_str))
+
+        selection = ButtonsTemplate(question, actions=actions)
+        selection_message = TemplateSendMessage(question, selection)
+
+        # カウントダウン画像メッセージ
+        image_url = "https://github.com/wakwakcreate/drink_scripts/raw/main/countdown.gif"
+        image_message = ImageSendMessage(image_url, image_url)
+
+        # メッセージを送信
+        line_bot_api.reply_message(
+            event.reply_token,
+            [selection_message, image_message])
+
 if __name__ == "__main__":
-    load_scripts()
     app.run()
