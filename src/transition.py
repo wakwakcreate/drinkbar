24791a2ab3f1b1dbabe32e5db8c88637ec02b6a6
wakwakcreaters <101112247+wakwakcreaters@users.noreply.github.com>
2022-05-09 23:44:46 +0900

    Update transition.py

--- src/transition.py
+++ src/transition.py
@@ -19,7 +19,7 @@ from .utils import get_drink_name, create_game_str_with_change, get_user_name, g
 def on_user_select():
     game_str = create_game_str_with_change({}, 'state', STATE_USER0_JOIN)
 
-    text = "一人目の参加者はボタンを押してね。"
+    text = "一人目の参加者はボタンを押してね。友達登録もしてね！"
     action = PostbackAction("参加", game_str)
     selection = ButtonsTemplate(text, actions=[action])
     selection_message = TemplateSendMessage(text, selection)
@@ -30,7 +30,7 @@ def on_user0_join(game, user_id):
     game['users'] = [{'id': user_id}]
     game_str = create_game_str_with_change(game, 'state', STATE_USER1_JOIN)
 
-    text = "二人目の参加者はボタンを押してね。"
+    text = "二人目の参加者はボタンを押してね。友達登録もしてね！"
     action = PostbackAction("参加", game_str)
     selection = ButtonsTemplate(text, actions=[action])
     selection_message = TemplateSendMessage(text, selection)
@@ -46,7 +46,7 @@ def on_user1_join(game, user_id):
     game['users'].append({'id': user_id})
     game_str = create_game_str_with_change(game, 'state', STATE_USER2_JOIN)
 
-    text = "三人目の参加者はボタンを押してね。"
+    text = "三人目の参加者はボタンを押してね。友達登録もしてね！"
     action = PostbackAction("参加", game_str)
     selection = ButtonsTemplate(text, actions=[action])
     selection_message = TemplateSendMessage(text, selection)
